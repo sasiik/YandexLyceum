096237c16c3af9be4523a1b82790117d51b0698b
sasiik <sstupko31@gmail.com>
2023-04-19 18:14:06 +0300

    Тренировки в полёте

--- main.py
+++ main.py
@@ -11,7 +11,13 @@ app = Flask(__name__)
 def index(title):
     return render_template('base.html', title=title)
 
+
+@app.route('/training/<prof>')
+def training(prof):
+    images = [url_for('static', filename="img/mars.png"), url_for('static', filename="img/riana.jpeg")]
+    return render_template('training.html', title=prof, prof=prof, images=images)
+
+
 if __name__ == '__main__':
     app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
     app.run(port=8080, host='127.0.0.1')
-
