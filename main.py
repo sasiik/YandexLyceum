07ca4c0358054262c1429cd161893fcebfbec7bf
sasiik <sstupko31@gmail.com>
2023-04-21 14:57:00 +0300

    Автоматический ответ

--- main.py
+++ main.py
@@ -24,6 +24,20 @@ def list_prof(list):
     return render_template('list_prof.html', title='Job list', list_type=list, profs=profs)
 
 
+@app.route('/answer')
+@app.route('/auto_answer')
+def answer():
+    form_data = {'title': 'Form',
+                 "surname": 'Wanty',
+                 "name": 'Mark',
+                 "education": 'School',
+                 "profession": 'Doctor',
+                 "sex": 'Male',
+                 "motivation": "Want to live on Mars",
+                 "ready": "Yes!"}
+    return render_template('answer.html', title=form_data['title'], form_data=form_data)
+
+
 if __name__ == '__main__':
     app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
     app.run(port=8080, host='127.0.0.1')
