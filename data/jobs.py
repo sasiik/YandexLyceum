c54466808e5af9ece647dd873f14fe380ceb572b
sasiik <sstupko31@gmail.com>
2023-04-24 12:17:39 +0300

    Первая работа

--- data/jobs.py
+++ data/jobs.py
@@ -18,7 +18,7 @@ class Jobs(SqlAlchemyBase):
     start_date = sqlalchemy.Column(sqlalchemy.DateTime,
                                    default=datetime.datetime.now)
     end_date = sqlalchemy.Column(sqlalchemy.DateTime,
-                                 default=datetime.datetime.now)
+                                 nullable=True)
     is_finished = sqlalchemy.Column(sqlalchemy.Boolean, nullable=True)
     leader = orm.relationship('User', back_populates='jobs')
 
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import datetime
 
 from flask import Flask, render_template, redirect
 from data import db_session
+from data.jobs import Jobs
 from data.users import User
 from data.news import News
 from forms.user import RegisterForm
@@ -65,8 +66,22 @@ def main():
             user.email = member[6]
             db_sess.add(user)
         db_sess.commit()
+    if not db_sess.query(Jobs).first():
+        job = Jobs()
+        job.team_leader = 1
+        job.job = 'deployment of residential modules 1 and 2'
+        job.work_size = 15
+        job.collaborators = '2, 3'
+        job.start_date = datetime.datetime.now()
+        job.is_finished = False
+        db_sess.add(job)
+        db_sess.commit()
+
     for user in db_sess.query(User).all():
         print(user)
+
+    for job in db_sess.query(Jobs).all():
+        print(job)
 
 
 if __name__ == '__main__':
